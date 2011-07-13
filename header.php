b49ae2644f1e10b61c9ed219e1af4a3b970f4983
Jared Lang <jslang@mail.ucf.edu>
2011-07-13 14:45:08 -0400

    Analytics to head.

--- header.php
+++ header.php
@@ -7,6 +7,10 @@
 		<style>article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {display: block;}</style>
 		<link href="http://cdn.ucf.edu/webcom/-/css/blueprint-ie.css" rel="stylesheet" media="screen, projection">
 		<![endif]-->
+		<script type="text/javascript">
+			var _sf_startpt=(new Date()).getTime();
+			var _gaq = _gaq || [];
+		</script>
 	</head>
 	<body class="<?=body_classes()?>">
 		<div id="blueprint-container" class="container">
