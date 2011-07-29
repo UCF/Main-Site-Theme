5250c06692c9763acd4255316e6d1b13b6cc8ba3
Jared Lang <jslang@mail.ucf.edu>
2011-07-29 10:01:41 -0400

    Added slideshow shortcode.

--- shortcodes.php
+++ shortcodes.php
@@ -10,6 +10,66 @@ add_shortcode('empty-shortcode', 'sc_empty_shortcode');
 
 
 /**
+ * Create a javascript slideshow of each top level element in the
+ * shortcode.  All attributes are optional, but may default to less than ideal
+ * values.  Available attributes:
+ * 
+ * height     => css height of the outputted slideshow, ex. height="100px"
+ * width      => css width of the outputted slideshow, ex. width="100%"
+ * transition => length of transition in milliseconds, ex. transition="1000"
+ * cycle      => length of each cycle in milliseconds, ex cycle="5000"
+ *
+ * Example:
+ * [slideshow height="500px" transition="500" cycle="2000"]
+ * <img src="http://some.image.com" .../>
+ * <div class="robots">Robots are coming!</div>
+ * <p>I'm a slide!</p>
+ * [/slideshow]
+ **/
+function sc_slideshow($attr, $content=null){
+	$content = cleanup(str_replace('<br />', '', $content));
+	$content = DOMDocument::loadHTML($content);
+	$html    = $content->childNodes->item(1);
+	$body    = $html->childNodes->item(0);
+	$content = $body->childNodes;
+	
+	# Find top level elements and add appropriate class
+	$items = array();
+	foreach($content as $item){
+		if ($item->nodeName != '#text'){
+			$classes   = explode(' ', $item->getAttribute('class'));
+			$classes[] = 'slide';
+			$item->setAttribute('class', implode(' ', $classes));
+			$items[] = $item->ownerDocument->saveXML($item);
+		}
+	}
+	
+	$height    = ($attr['height']) ? $attr['height'] : '100px';
+	$width     = ($attr['width']) ? $attr['width'] : '100%';
+	$tran_len  = ($attr['transition']) ? $attr['transition_length'] : 1000;
+	$cycle_len = ($attr['cycle']) ? $attr['cycle_length'] : 5000;
+	
+	ob_start();
+	?>
+	<div 
+		class="slideshow"
+		data-tranlen="<?=$tran_len?>"
+		data-cyclelen="<?=$cycle_len?>"
+		style="height: <?=$height?>; width: <?=$width?>;"
+	>
+		<?php foreach($items as $item):?>
+		<?=$item?>
+		<?php endforeach;?>
+	</div>
+	<?php
+	$html = ob_get_clean();
+	
+	return $html;
+}
+add_shortcode('slideshow', 'sc_slideshow');
+
+
+/**
  * Fetches objects defined by arguments passed, outputs the objects according
  * to the toHTML method located on the object.
  **/
