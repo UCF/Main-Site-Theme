bf8df46bbc0df08a189c6c356f1487040e5c14a1
Jared Lang <jslang@mail.ucf.edu>
2011-11-30 12:38:21 -0500

    Fixed: missing defined var for theme includes directory.

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@
 define('DEBUG', isset($_GET['debug'])); # Enable via get parameter
 define('THEME_URL', get_bloginfo('stylesheet_directory'));
 define('THEME_DIR', get_stylesheet_directory());
+define('THEME_INCLUDES_DIR', THEME_DIR.'/includes');
 define('THEME_STATIC_URL', THEME_URL.'/static');
 define('THEME_IMG_URL', THEME_STATIC_URL.'/img');
 define('THEME_JS_URL', THEME_STATIC_URL.'/js');
