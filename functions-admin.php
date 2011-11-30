c8b561c8f1906f572fa003502999bb13f4e3e227
Jared Lang <jslang@mail.ucf.edu>
2011-11-30 12:24:31 -0500

    Moved theme options html out of function declaration and into more manageable theme options template file.

--- functions-admin.php
+++ functions-admin.php
@@ -60,56 +60,7 @@ function create_theme_options_page() {
  * @author Jared Lang
  **/
 function theme_options_page(){
-	# Check for settings updated or updated, varies between wp versions
-	$updated = (bool)($_GET['settings-updated'] or $_GET['updated']);
-	?>
-	
-	<form method="post" action="options.php" id="theme-options">
-		<div class="wrap">
-			<h2><?=__(THEME_OPTIONS_PAGE_TITLE)?></h2>
-			
-			<?php if ($updated):?>
-			<div class="updated fade"><p><strong><?=__( 'Options saved' ); ?></strong></p></div>
-			<?php endif; ?>
-			
-			<?php settings_fields(THEME_OPTIONS_GROUP);?>
-			<table class="form-table">
-				<?php foreach(Config::$theme_settings as $key=>$setting):?>
-				<?php if(is_array($setting)): $section = $setting;?>
-				<tr class="section">
-					<td colspan="2">
-						<h3><?=$key?></h3>
-						<table class="form-table">
-							<?php foreach($section as $setting):?>
-							<tr valign="top">
-								<th scope="row"><?=$setting->label_html()?></th>
-								<td class="field">
-									<?=$setting->input_html()?>
-									<?=$setting->description_html()?>
-								</td>
-							</tr>
-							<?php endforeach;?>
-						</table>
-					</td>
-				</tr>
-				<?php else:?>
-				<tr valign="top">
-					<th scope="row"><?=$setting->label_html()?></th>
-					<td class="field">
-						<?=$setting->input_html()?>
-						<?=$setting->description_html()?>
-					</td>
-				</tr>
-				<?php endif;?>
-				<?php endforeach;?>
-			</table>
-			<div class="submit">
-				<input type="submit" class="button-primary" value="<?= __('Save Options')?>" />
-			</div>
-		</div>
-	</form>
-	
-	<?php
+	include(THEME_INCLUDES_DIR.'/theme-options.php');
 }
 
 
