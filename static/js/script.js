01adc863ba8a3c09b77a78518457c32c38d9d562
Chris Conover <clcono@gmail.com>
2012-02-06 11:46:18 -0500

    Red-added some assets from original generic theme

---
+++ static/js/script.js
@@ -0,0 +1,91 @@
+var Generic = {};
+
+Generic.resizeSearch = function($){
+	var form          = $('.search-form');
+	var height        = form.height();
+	var search_field  = form.find('.search-field');
+	var search_button = form.find('.search-submit');
+	
+	var loops = 0;
+	
+	while (form.height() == height){
+		var width = search_field.width();
+		search_field.width(++width);
+		
+		loops++;
+		if (loops > 1024){break;}
+	}
+	search_field.width(search_field.width() - 1);
+};
+
+Generic.homeDimensions = function($){
+	var cls = this;
+	cls.home_element = $('#home');
+	
+	cls.resizeToHeight = function(element, target_height){
+		if(element.length < 1){return;}
+		
+		var loops = 0;
+		
+		var difference = function(){
+			return Math.abs(element.height() - target_height);
+		};
+		
+		// Adjust smaller if the text is too large
+		while (element.height() > target_height){
+			var current_font_size = parseInt(element.css('font-size'));
+			element.css('font-size', --current_font_size + 'px');
+			if (current_font_size < 10){
+				break;
+			}
+			if (++loops > 1024){break;}
+		}
+		
+		
+		// Adjust larger if the text is too small
+		while (element.height() < target_height && difference() > 8){
+			var current_font_size = parseInt(element.css('font-size'));
+			element.css('font-size', ++current_font_size + 'px');
+			if (element.height() > target_height && difference() > 8){
+				element.css('font-size', --current_font_size + 'px');
+				break;
+			}
+			if (++loops > 1024){break;}
+		}
+		
+		element.height(target_height);
+	};
+	
+	cls.uniformHeight = function(){
+		var template = cls.home_element.data()['template'];
+		
+		if (template == "home-nodescription"){
+			cls.resizeToHeight($('.content'), $('.site-image').height());
+		}
+		
+		if (template == "home-description"){
+			cls.resizeToHeight($('.right-column .description'), $('.site-image').height() - $('.search').height());
+		}
+		return;
+	};
+	
+	if (cls.home_element.length < 1){return;}
+	
+	cls.uniformHeight();
+	
+	
+};
+
+if (typeof jQuery != 'undefined'){
+	jQuery(document).ready(function($) {
+		Webcom.slideshow($);
+		Webcom.chartbeat($);
+		Webcom.analytics($);
+		Webcom.handleExternalLinks($);
+		Webcom.loadMoreSearchResults($);
+		
+		/* Theme Specific Code Here */
+		Generic.homeDimensions($);
+		Generic.resizeSearch($);
+	});
+}else{console.log('jQuery dependancy failed to load');}
