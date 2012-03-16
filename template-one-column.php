fc44e2c332252ad2d0543a92e88d02cb1dab85f1
Christopher Conover <clcono@gmail.com>
2012-03-16 16:48:14 -0400

    Fixed issue on one column template

--- template-one-column.php
+++ template-one-column.php
@@ -4,17 +4,17 @@
  **/
 ?>
 <?php get_header(); the_post();?>
-	
-	<div class="span12 page-content" id="<?=$post->post_name?>">
-		<article>
-			<h2><?php the_title();?></h2>
-			<?php the_content();?>
-		</article>
-		<?
-		if(!get_post_meta($post->ID, 'page_hide_fold', True)):
-			get_template_part('includes/below-the-fold');
-		endif
-		?>
+	<div class="row page-content" id="<?=$post->post_name?>">
+		<div class="span12">
+			<article>
+				<h1><?php the_title();?></h1>
+				<?php the_content();?>
+			</article>
+		</div>
 	</div>
-
+	<?
+	if(get_post_meta($post->ID, 'page_hide_fold', True) != 'on'): 
+		get_template_part('includes/below-the-fold'); 
+	endif
+	?>
 <?php get_footer();?>
