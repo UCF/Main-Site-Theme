4b1ebf1bd9a62ee9453c994af8695d46404c8172
Chris Conover <clcono@gmail.com>
2012-04-26 10:29:47 -0400

    Do not display h1's on pages marked as the front page

--- template-two-column.php
+++ template-two-column.php
@@ -7,7 +7,9 @@
 	<div class="row page-content" id="<?=$post->post_name?>">
 		<div class="span9">
 			<article>
-				<h1><?php the_title();?></h1>
+				<? if(!is_front_page())	{ ?>
+						<h1><?php the_title();?></h1>
+				<? } ?>
 				<?php the_content();?>
 			</article>
 		</div>
