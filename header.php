46df4b13b61c021707dbfc4eb65c4633b874d700
Jo Greybill <jogreybill@gmail.com>
2012-07-02 16:00:26 -0400

    Added code which inserts custom stylesheets in header

--- header.php
+++ header.php
@@ -28,6 +28,12 @@
 		</script>
 		<?php endif;?>
 		
+		<?  $post_type = get_post_type($post->ID);
+			if(($stylesheet_id = get_post_meta($post->ID, $post_type.'_stylesheet', True)) !== False
+				&& ($stylesheet_url = wp_get_attachment_url($stylesheet_id)) !== False) { ?>
+				<link rel='stylesheet' href="<?=$stylesheet_url?>" type='text/css' media='all' />
+		<? } ?>
+		
 	</head>
 	<body class="<?=body_classes()?>">
 		<div class="container">
