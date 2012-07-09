0961a6c065ed92a9804840ed90a6c4dd158e0de2
Jo Greybill <jogreybill@gmail.com>
2012-07-09 15:34:41 -0400

    Added script that dynamically removes gravityform's stylesheet if it was added automatically by gravityforms (if the Output CSS settings weren't set to 'no' in GF's settings)

--- static/js/script.js
+++ static/js/script.js
@@ -83,6 +83,14 @@ Generic.defaultMenuSeparators = function($) {
 	$('.menu.horizontal li:last-child').addClass('last');
 };
 
+Generic.removeExtraGformStyles = function($) {
+	// Since we're re-registering the Gravity Form stylesheet
+	// manually and we can't dequeue the stylesheet GF adds
+	// by default, we're removing the reference to the script if
+	// it exists on the page (if CSS hasn't been turned off in GF settings.)
+	$('link#gforms_css-css').remove();
+}
+
 
 if (typeof jQuery != 'undefined'){
 	jQuery(document).ready(function($) {
@@ -96,5 +104,6 @@ if (typeof jQuery != 'undefined'){
 		//Generic.homeDimensions($);
 		//Generic.resizeSearch($);
 		Generic.defaultMenuSeparators($);
+		Generic.removeExtraGformStyles($);
 	});
 }else{console.log('jQuery dependancy failed to load');}
