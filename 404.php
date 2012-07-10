905940f7288e2128a23811632e5dbbf758ebf6e2
Jo Greybill <jogreybill@gmail.com>
2012-07-10 10:12:39 -0400

    Updated to use Bootstrap styles

--- 404.php
+++ 404.php
@@ -2,30 +2,29 @@
 <?php disallow_direct_load('404.php');?>
 
 <?php get_header(); the_post();?>
-	<div class="page-content" id="page-not-found">
-		<div class="span-18">
-			<h1>Page Not Found</h1>
-			<?php 
-				$page = get_page_by_title('404');
-				if($page){
-					$content = $page->post_content;
-					$content = apply_filters('the_content', $content);
-					$content = str_replace(']]>', ']]>', $content);
-				}
-			?>
-			<?php if($content):?>
-			<?=$content?>
-			<?php else:?>
-			<p>The page you requested doesn't exist.  Sorry about that.</p>
-			<?php endif;?>
+	<div class="row page-content" id="page-not-found">
+		<div class="span9">
+			<article>
+				<h1>Page Not Found</h1>
+				<?php 
+					$page = get_page_by_title('404');
+					if($page){
+						$content = $page->post_content;
+						$content = apply_filters('the_content', $content);
+						$content = str_replace(']]>', ']]>', $content);
+					}
+				?>
+				<?php if($content):?>
+				<?=$content?>
+				<?php else:?>
+				<p>The page you requested doesn't exist.  Sorry about that.</p>
+				<?php endif;?>
+			</article>
 		</div>
 		
-		<div id="sidebar" class="span-6 last">
+		<div id="sidebar" class="span3">
 			<?=get_sidebar();?>
 		</div>
-		
-		<div class="clear"><!-- --></div>
-		<?php get_template_part('includes/below-the-fold'); ?>
 	</div>
-
+	<?php get_template_part('includes/below-the-fold'); ?>
 <?php get_footer();?>
