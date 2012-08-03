16ab1811f857ddc3eb435c96820f529f8bd72393
Jo Greybill <jogreybill@gmail.com>
2012-08-03 17:24:04 -0400

    Switch out nav menu for bootstrap static navbar at 480px wide or less

--- static/js/script.js
+++ static/js/script.js
@@ -91,6 +91,42 @@ Generic.removeExtraGformStyles = function($) {
 	$('link#gforms_css-css').remove();
 }
 
+Generic.mobileNavBar = function($) {
+	// Switch the navigation bar from standard horizontal nav to bootstrap mobile nav
+	// when the browser is at mobile size:
+	var mobile_wrap = function() {
+		$('#header-menu').wrap('<div class="navbar"><div class="navbar-inner"><div class="container" id="mobile_dropdown_container"><div class="nav-collapse"></div></div></div></div>');
+		$('<a class="btn btn-navbar" id="mobile_dropdown_toggle" data-target=".nav-collapse" data-toggle="collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a><a class="brand" href="#">Navigation</a>').prependTo('#mobile_dropdown_container');
+		$('.current-menu-item, .current_page_item').addClass('active');
+	}
+	var mobile_unwrap = function() {
+		$('#mobile_dropdown_toggle .icon-bar').remove();
+		$('#mobile_dropdown_toggle').remove();
+		$('#mobile_dropdown_container a.brand').remove();
+		$('#header-menu').unwrap();
+		$('#header-menu').unwrap();
+		$('#header-menu').unwrap();
+		$('#header-menu').unwrap();
+	}
+	var adjust_mobile_nav = function() {
+		if ($(window).width() < 480) {
+			if ($('#mobile_dropdown_container').length < 1) {
+				mobile_wrap();
+			}
+		}
+		else {
+			if ($('#mobile_dropdown_container').length > 0) {
+				mobile_unwrap();
+			}
+		}
+	}
+	
+	adjust_mobile_nav();
+	$(window).resize(function() {
+		adjust_mobile_nav();
+	});
+}
+
 
 if (typeof jQuery != 'undefined'){
 	jQuery(document).ready(function($) {
@@ -104,5 +140,6 @@ if (typeof jQuery != 'undefined'){
 		//Generic.resizeSearch($);
 		Generic.defaultMenuSeparators($);
 		Generic.removeExtraGformStyles($);
+		Generic.mobileNavBar($);
 	});
 }else{console.log('jQuery dependancy failed to load');}
