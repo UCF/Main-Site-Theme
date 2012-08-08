29e916c8daf619b9320ec8ea79ec129db3e7c116
Jo Greybill <jogreybill@gmail.com>
2012-08-08 09:37:18 -0400

    Added All Sliders screen customizations from SmartStart

--- functions.php
+++ functions.php
@@ -9,4 +9,56 @@ require_once('shortcodes.php');         		# Per theme shortcodes
 
 //Add theme-specific functions here.
 
+
+/** 
+ * Slider post type customizations 
+ * Stolen from SmartStart theme
+ **/
+
+// Custom columns for 'Slider' post type
+function ss_framework_edit_slider_columns() {
+	$columns = array(
+		'cb'          => '<input type="checkbox" />',
+		'title'       => __( 'Name' ),
+		'slide_count' => __( 'Slide Count' ),
+		'shortcode'   => __( 'Shortcode' )
+	);
+	return $columns;
+}
+add_action('manage_edit-slider_columns', 'ss_framework_edit_slider_columns');
+
+// Custom columns content for 'Slider'
+function ss_framework_manage_slider_columns( $column, $post_id ) {
+	global $post;
+	switch ( $column ) {
+		case 'slide_count':
+			$slider_slides = get_post_meta( $post->ID, $id, true ) ? get_post_meta( $post->ID, $id, true ) : false;
+			$slide_count = count( unserialize( $slider_slides['ss_slider_slides'][0] ) );
+			echo $slide_count;
+			break;
+		case 'shortcode':
+			echo '<span class="shortcode-field">[slider id="'. $post->post_name . '"]</span>';
+			break;
+		default:
+			break;
+	}
+}
+add_action('manage_slider_posts_custom_column', 'ss_framework_manage_slider_columns', 10, 2);
+
+// Sortable custom columns for 'Slider'
+function ss_framework_sortable_slider_columns( $columns ) {
+	$columns['slide_count'] = 'slide_count';
+	return $columns;
+}
+add_action('manage_edit-slider_sortable_columns', 'ss_framework_sortable_slider_columns');
+
+// Change default title for 'Slider'
+function ss_framework_change_slider_title( $title ){
+	$screen = get_current_screen();
+	if ( $screen->post_type == 'slider' )
+		$title = __('Enter slider name here');
+	return $title;
+}
+add_filter('enter_title_here', 'ss_framework_change_slider_title');
+
 ?>
