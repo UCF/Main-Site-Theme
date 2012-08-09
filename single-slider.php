ff05d5fdb399cf126d158e391596ba7a620a6e57
Jo Greybill <jogreybill@gmail.com>
2012-08-09 10:59:16 -0400

    Updated template structure

--- single-slider.php
+++ single-slider.php
@@ -2,24 +2,14 @@
 
 <?php get_header(); ?>
 
-<section id="content" class="clearfix">
-
-	<div class="container">
-
-		<header class="page-header">
-
-			<h1 class="page-title"><?php the_title(); ?></h1>
-
-		</header><!-- end .page-header -->
+	<div class="row page-content nodescription">
 
 		<?php echo do_shortcode('[slider id="' . $post->post_name . '"]'); ?>
-
-		<div class="clear"></div>
-
+		<div style="clear:both;"></div>
 		<p><?php edit_post_link(__('Edit', 'ss_framework'), '', ''); ?></p>
-	
-	</div><!-- end .container -->
-	
-</section><!-- end #content -->
+	</div>
+</div>
+<div class="container">
+	<div class="row">
 
-<?php get_footer(); ?>
+		<?php get_footer(); ?>
