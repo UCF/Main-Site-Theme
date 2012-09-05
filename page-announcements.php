14358909e9ddf746502c52dae2e061a0bfa190cd
Jo Greybill <jogreybill@gmail.com>
2012-09-05 08:55:53 -0400

    Created new page for announcement output; updated styles

---
+++ page-announcements.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Template Name: One Column
+ **/
+?>
+<?php get_header(); the_post();?>
+	<div class="row page-content" id="<?=$post->post_name?>">
+		<div class="span12" id="page_title">
+			<h1 class="span9"><?php the_title();?></h1>
+			<?=output_weather_data('span3')?>
+		</div>
+		
+		<div class="span12" id="contentcol">
+			<article>
+				<h2 class="header_announcements">Announcements for this week | <a href="http://events.ucf.edu/">Go to Events &raquo;</a></h2>
+				<div class="row" id="filters">
+					<div class="span4" id="filter_wrap">
+						<label for="filter">Filter Results by...</label>
+						<div class="btn-group" id="filter" data-toggle="buttons-radio">
+							<button type="button" id="filter_audience" class="btn active">Audience</button>
+							<button type="button" id="filter_keyword" class="btn">Keyword</button>
+							<button type="button" id="filter_time" class="btn">Time</button>
+						</div>
+					</div>
+					
+					<div class="span3">	
+						<label for="f_role">Select an Audience</label>
+						<select name="f_role" class="span3">
+							<option selected="" value="all">All Roles</option>
+							<option value="alumni">Alumni</option>
+							<option value="faculty">Faculty</option>
+							<option value="prospective">Prospective Students</option>
+							<option value="gpublic">Public</option>
+							<option value="staff">Staff</option>
+							<option value="student">Students</option>
+						</select>
+					</div>
+					<div class="span3">	
+						<label for="f_keyword">Type a Keyword</label>
+						<input type="text" name="f_keyword" class="span3" />
+					</div>
+					<div class="span3">	
+						<label for="f_duration">Select a Time</label>
+						<select name="f_duration" class="span3">
+							<option selected="" value="thisweek">This Week</option>
+							<option value="nextweek">Next Week</option>
+							<option value="thismonth">This Month</option>
+							<option value="nextmonth">Next Month</option>
+							<option value="thissemester">This Semester</option>
+							<option value="all">All</option>
+						</select>
+					</div>
+					
+					<div class="span1">	
+						<input type="submit" class="btn" value="View" id="filter_update">
+					</div>
+					<div class="span3" id="addnew_wrap">
+						<a class="btn btn-primary" id="addnew_announcement" href="post-an-announcement">Post an Announcement</a>
+					</div>
+				</div>
+				
+				<?php the_content();?>
+				<p>Announcements will go here...</p>
+			</article>
+		</div>
+	</div>
+<?php get_footer();?>
