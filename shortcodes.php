e1da7583415cfcd62e88d3452cc4353898735a62
Jo Greybill <jogreybill@gmail.com>
2012-09-18 15:31:18 -0400

    Re-added content editor for Publications; added content output as single pub description

--- shortcodes.php
+++ shortcodes.php
@@ -99,8 +99,10 @@ function sc_publication($attr, $content=null){
 	
 	<div class="pub">
 		<a class="track pub-track" title="<?=$pub->post_title?>" data-toggle="modal" href="#pub-modal-<?=$pub->ID?>">
-			<?=$pub->thumb?><span><?=$pub->post_title?></span>
+			<?=$pub->thumb?>
+			<span><?=$pub->post_title?></span>
 		</a>
+		<p class="pub-desc"><?=$pub->post_content?></p>
 		<div class="modal hide fade" id="pub-modal-<?=$pub->ID?>" role="dialog" aria-labelledby="<?=$pub->post_title?>" aria-hidden="true">
 			<iframe src="<?=$pub->url?>" width="100%" height="100%" scrolling="no"></iframe>
 			<a href="#" class="btn" data-dismiss="modal">Close</a>
