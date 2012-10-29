3f09ccd62e01b07875c687aae39f1cdc8889d548
Chris Conover <clcono@gmail.com>
2012-10-29 15:45:50 -0400

    Dropped default templates. Unregister sidebars.

--- home.php
+++
@@ -1,6 +0,0 @@
-<?php $options = get_option(THEME_OPTIONS_NAME);?>
-<?php if($options['site_description']):?>
-<?php include_once('template-home-description.php');?>
-<?php else:?>
-<?php include_once('template-home-nodescription.php');?>
-<?php endif;?>
--- page.php
+++
@@ -1,1 +0,0 @@
-<?php include_once('template-one-column.php');?>
