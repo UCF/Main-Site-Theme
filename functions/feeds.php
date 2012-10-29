fd5986c9c7744fcbc2d0af5b395f0340ed31b217
Chris Conover <clcono@gmail.com>
2012-10-29 15:17:26 -0400

    Removed SimplePie require dependency

--- functions/feeds.php
+++ functions/feeds.php
@@ -69,7 +69,7 @@ class FeedManager{
 	 **/
 	static protected function __new_feed($url){
 		$timer = Timer::start();
-		require_once(THEME_DIR.'/third-party/simplepie.php');
+		require_once(ABSPATH . WPINC . '/class-feed.php');
 		
 		$simplepie = null;
 		$failed    = False;
