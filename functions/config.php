9b0d121b06b2b4b6c609f66b8cae4d19a6efb442
Jo Greybill <jogreybill@gmail.com>
2012-11-08 08:32:44 -0500

    Updated bootstrap file directory paths

--- functions/config.php
+++ functions/config.php
@@ -329,12 +329,12 @@ Config::$links = array(
 Config::$styles = array(
 	array('admin' => True, 'src' => THEME_CSS_URL.'/admin.css',),
 	'http://universityheader.ucf.edu/bar/css/bar.css',
-	THEME_STATIC_URL.'/bootstrap/build/css/bootstrap.css',
+	THEME_STATIC_URL.'/bootstrap/bootstrap/css/bootstrap.css',
 );
 
 if ($theme_options['bootstrap_enable_responsive'] == 1) {
 	array_push(Config::$styles, 
-		THEME_STATIC_URL.'/bootstrap/build/css/bootstrap-responsive.css'
+		THEME_STATIC_URL.'/bootstrap/bootstrap/css/bootstrap-responsive.css'
 	);		
 }
 
@@ -354,7 +354,7 @@ Config::$scripts = array(
 	array('admin' => True, 'src' => THEME_JS_URL.'/admin.js',),
 	'http://universityheader.ucf.edu/bar/js/university-header.js',
 	//array('name' => 'jquery', 'src' => 'http://code.jquery.com/jquery-1.7.1.min.js',),
-	THEME_STATIC_URL.'/bootstrap/build/js/bootstrap.js',
+	THEME_STATIC_URL.'/bootstrap/bootstrap/js/bootstrap.js',
 	THEME_JS_URL.'/jquery.backgroundSize.js',
 	THEME_JS_URL.'/cycle.lite.js',
 	array('name' => 'base-script',  'src' => THEME_JS_URL.'/webcom-base.js',),
