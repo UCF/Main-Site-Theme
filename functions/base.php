84770b281eb8a54273d6d0ce79a263379f926b9b
Jo Dickson <jogreybill@gmail.com>
2014-03-24 16:10:15 -0400

    Actually get rid of Degree post type stuff

--- functions/base.php
+++ functions/base.php
@@ -1297,17 +1297,11 @@ function header_links(){
  * Generates a title based on context page is viewed.  Stolen from Thematic
  **/
 function header_title(){
-	global $post;
 	$site_name = get_bloginfo('name');
 	$separator = '|';
 
 	if ( is_single() ) {
-		if ($post->post_type == 'degree') {
-			$content = 'Degree Program | '.single_post_title('', FALSE);
-		}
-		else {
-			$content = single_post_title('', FALSE);
-		}
+		$content = single_post_title('', FALSE);
 	}
 	elseif ( is_home() || is_front_page() ) { 
 		$content = get_bloginfo('description');
