7c54ebdd4fed3e987a02035f55da0fda747d1bbc
Jim Barnes <james.barnes@ucf.edu>
2015-03-18 09:17:04 -0400

    Updated events feed urls.

--- shortcodes.php
+++ shortcodes.php
@@ -383,8 +383,8 @@ add_shortcode('person-picture-list', 'sc_person_picture_list');
  **/
 function sc_events_widget() {
 	display_events();
-	print '<p class="events_icons"><a class="icsbtn" href="http://events.ucf.edu/?upcoming=upcoming&format=ics">ICS Format for upcoming events</a><a class="rssbtn" href="http://events.ucf.edu/?upcoming=upcoming&format=rss">RSS Format for upcoming events</a></p>
-	<p class="screen-only"><a href="http://events.ucf.edu/?upcoming=upcoming" class="events_morelink">More Events</a></p>';
+	print '<p class="events_icons"><a class="icsbtn" href="http://events.ucf.edu/upcoming/feed.ics">ICS Format for upcoming events</a><a class="rssbtn" href="http://events.ucf.edu/upcoming/feed.rss">RSS Format for upcoming events</a></p>
+	<p class="screen-only"><a href="http://events.ucf.edu/upcoming/" class="events_morelink">More Events</a></p>';
 }
 add_shortcode('events-widget', 'sc_events_widget');
 
