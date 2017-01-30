46546cf3cee185bf20e2be57850a47da8083ce87
Jim Barnes <james.barnes@ucf.edu>
2017-01-30 10:31:19 -0500

    Updates per peer review.

--- dev/custom-pages/azindex.js
+++ dev/custom-pages/azindex.js
@@ -1,7 +1,6 @@
 (function() {
 
-    var $jumpTo       = $('#jump-to'),
-        $alphaResults = $('.post-type-search-alpha'),
+    var $alphaResults = $('.post-type-search-alpha'),
         toTopMarkup   = '<span class="to-top-text"><span class="fa fa-long-arrow-up"></span> <a href="#">Back to Top</a></span>';
 
 
@@ -62,20 +61,14 @@
       $letterHeading
         .after(toTopMarkup);
 
-      if ($letterList.children().length < 1) {
-        // Disable jump-to options for sections with no content
-        $jumpTo.find('option[value="az-'+ letter +'"]').attr('disabled', true);
-      }
-      else {
-        // Apply affixing to each heading wrap, if that letter has list items.
-        letterWrapAffix.offset = getLetterWrapAffixOffset($letterWrap, $letterList);
-        $letterWrap.affix(letterWrapAffix);
+      // Apply affixing to each heading wrap, if that letter has list items.
+      letterWrapAffix.offset = getLetterWrapAffixOffset($letterWrap, $letterList);
+      $letterWrap.affix(letterWrapAffix);
 
-        // Update affix offsets on window resize.
-        $(window).on('resize', debounce( function() {
-          $letterWrap.data('bs.affix').options.offset = getLetterWrapAffixOffset($letterWrap, $letterList);
-        }, 100));
-      }
+      // Update affix offsets on window resize.
+      $(window).on('resize', debounce( function() {
+        $letterWrap.data('bs.affix').options.offset = getLetterWrapAffixOffset($letterWrap, $letterList);
+      }, 100));
 
     });
 
