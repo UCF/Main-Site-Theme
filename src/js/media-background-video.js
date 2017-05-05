0c4cbfc04930790c0fc269b7d48819a534d1422e
Jo Dickson <jogreybill@gmail.com>
2017-05-05 13:18:26 -0500

    Don't run media-background-video logic if at least one background video and toggle btn are present in the dom

--- src/js/media-background-video.js
+++ src/js/media-background-video.js
@@ -61,28 +61,30 @@
   }
 
   function init() {
-    // Set the autoplay cookie if it hasn't been set yet
-    if (typeof Cookies.get('ucfedu-autoplay-bg-videos') === 'undefined') {
-      Cookies.set('ucfedu-autoplay-bg-videos', '1', {
-        expires: 365
-      });
+    if ($videos.length && $btns.length) {
+      // Set the autoplay cookie if it hasn't been set yet
+      if (typeof Cookies.get('ucfedu-autoplay-bg-videos') === 'undefined') {
+        Cookies.set('ucfedu-autoplay-bg-videos', '1', {
+          expires: 365
+        });
+      }
+
+      // Toggle the btn and video play on load
+      if (Cookies.get('ucfedu-autoplay-bg-videos') === '1') {
+        toggleBtnPlay();
+      } else {
+        toggleBtnPause();
+        bgVideosPause();
+      }
+
+      // Show the toggle btn and apply event handling
+      $btns
+        .removeClass('hidden-xs-up')
+        .on('click', btnClickHandler);
+
+      // Reset the btn controls when the first video on the page ends
+      $videos.get(0).addEventListener('ended', toggleBtnPause, false);
     }
-
-    // Toggle the btn and video play on load
-    if (Cookies.get('ucfedu-autoplay-bg-videos') === '1') {
-      toggleBtnPlay();
-    } else {
-      toggleBtnPause();
-      bgVideosPause();
-    }
-
-    // Show the toggle btn and apply event handling
-    $btns
-      .removeClass('hidden-xs-up')
-      .on('click', btnClickHandler);
-
-    // Reset the btn controls when the first video on the page ends
-    $videos.get(0).addEventListener('ended', toggleBtnPause, false);
   }
 
   $(init);
