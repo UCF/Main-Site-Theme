5708c7108838e81227491fe8913c9147d8d573b6
Jo Dickson <jogreybill@gmail.com>
2017-06-20 14:49:21 -0500

    Added alert above header

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 		<?php wp_head(); ?>
 	</head>
 	<body ontouchstart <?php body_class(); ?>>
+		<?php if ( class_exists( 'UCF_Alert_Common' ) ) { echo UCF_Alert_Common::display_alert();  } ?>
 		<header>
 			<?php echo get_header_markup(); ?>
 		</header>
