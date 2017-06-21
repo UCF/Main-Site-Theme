6bc13c3856eb0acdda81b86c6039c1dd5352a38e
Jo Dickson <jogreybill@gmail.com>
2017-06-21 13:48:55 -0500

    Moved alert-related functions to a separate file; added forced option setting and asset dependency handling for alerts

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ include_once 'includes/header-functions.php';
 include_once 'includes/utilities.php';
 
 include_once 'includes/degree-functions.php';
+include_once 'includes/ucf-alert-functions.php';
 
 /**
  * Enqueues assets for a particular page
@@ -240,81 +241,3 @@ function add_section_markup( $output, $section ) {
 }
 
 add_filter( 'ucf_section_display', 'add_section_markup', 10, 2 );
-
-
-/**
- * Add custom alert layout for main site
- **/
-if ( class_exists( 'UCF_Alert_Common' ) ) {
-
-	function mainsite_alert_get_layouts( $layouts ) {
-		$layouts = array_merge(
-			$layouts,
-			array(
-				'faicon' => 'Icon Layout (req\'s FontAwesome and Athena Framework)'
-			)
-		);
-		return $layouts;
-	}
-
-	add_filter( 'ucf_alert_get_layouts', 'mainsite_alert_get_layouts' );
-
-	function mainsite_alert_display_faicon_before() {
-		$id = UCF_Alert_Common::get_alert_wrapper_id();
-		ob_start();
-	?>
-		<div data-script-id="<?php echo $id; ?>" class="ucf-alert-wrapper"></div>
-		<script type="text/html" id="<?php echo $id; ?>">
-			<div class="alert ucf-alert ucf-alert-faicon" data-alert-id="" role="alert">
-	<?php
-		echo ob_get_clean();
-	}
-
-	add_action( 'ucf_alert_display_faicon_before', 'mainsite_alert_display_faicon_before', 10, 0 );
-
-	function mainsite_alert_display_faicon() {
-		ob_start();
-	?>
-		<div class="container">
-			<div class="row no-gutters">
-				<div class="col col-lg-8 offset-lg-2">
-					<a class="ucf-alert-content" href="<?php echo UCF_Alert_Config::get_option_or_default( 'alerts_url' ); ?>">
-						<div class="row no-gutters">
-							<div class="col-auto">
-								<span class="ucf-alert-icon fa" aria-hidden="true"></span>
-							</div>
-							<div class="col">
-								<strong class="ucf-alert-title h4"></strong>
-								<div class="ucf-alert-body"></div>
-								<div class="ucf-alert-cta">
-									<?php echo UCF_Alert_Config::get_option_or_default( 'cta' ); ?>
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div class="col-auto">
-					<button type="button" class="ucf-alert-close close" aria-label="Close alert">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-			</div>
-		</div>
-	<?php
-		echo ob_get_clean();
-	}
-
-	add_action( 'ucf_alert_display_faicon', 'mainsite_alert_display_faicon', 10, 0 );
-
-	function mainsite_alert_display_faicon_after() {
-		ob_start();
-	?>
-			</div>
-		</script>
-	<?php
-		echo ob_get_clean();
-	}
-
-	add_action( 'ucf_alert_display_faicon_after', 'mainsite_alert_display_faicon_after', 10, 0 );
-
-}
