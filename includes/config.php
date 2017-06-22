7f50744edce8e77f6a5c8258425d1613bf70626c
Jo Dickson <jogreybill@gmail.com>
2017-06-22 10:41:18 -0500

    Added settings and template handling logic from main-site-theme v2

--- includes/config.php
+++ includes/config.php
@@ -87,6 +87,69 @@ function add_id_to_ucfhb( $url ) {
 add_filter( 'clean_url', 'add_id_to_ucfhb', 10, 1 );
 
 
+/**
+ * Generates a title based on context page is viewed.  Stolen from Thematic
+ **/
+function header_title( $title, $separator ) {
+	$site_name = get_bloginfo( 'name' );
+	if ( is_single() ) {
+		$content = single_post_title( '', false );
+	}
+	elseif ( is_home() || is_front_page() ) {
+		$content = get_bloginfo( 'description' );
+	}
+	elseif ( is_page() ) {
+		$content = single_post_title( '', false );
+	}
+	elseif ( is_search() ) {
+		$content = __( 'Search Results for:' );
+		$content .= ' ' . esc_html( stripslashes( get_search_query() ) );
+	}
+	elseif ( is_category() ) {
+		$content = __( 'Category Archives:' );
+		$content .= ' ' . single_cat_title( '', false );
+	}
+	elseif ( is_404() ) {
+		$content = __( 'Not Found' );
+	}
+	else {
+		$content = get_bloginfo( 'description' );
+	}
+	if ( get_query_var( 'paged' ) ) {
+		$content .= ' ' .$separator. ' ';
+		$content .= 'Page';
+		$content .= ' ';
+		$content .= get_query_var( 'paged' );
+	}
+	if ( $content ) {
+		if ( is_home() || is_front_page() ) {
+			$elements = array(
+				'site_name' => $site_name,
+				'separator' => $separator,
+				'content' => $content,
+			);
+		} else {
+			$elements = array(
+				'content' => $content,
+			);
+		}
+	} else {
+		$elements = array(
+			'site_name' => $site_name,
+		);
+	}
+	// But if they don't, it won't try to implode
+	if ( is_array( $elements ) ) {
+		$doctitle = implode( ' ', $elements );
+	}
+	else {
+		$doctitle = $elements;
+	}
+	return $doctitle;
+}
+add_filter( 'wp_title', 'header_title', 10, 2 );
+
+
 function define_customizer_sections( $wp_customize ) {
 	$wp_customize->add_section(
 		THEME_CUSTOMIZER_PREFIX . 'degrees',
@@ -217,6 +280,7 @@ add_action( 'customize_register', 'define_customizer_fields' );
  **/
 function custom_mimes( $mimes ) {
 	$mimes['svg'] = 'image/svg+xml';
+	$mimes['json'] = 'application/json';
 
 	return $mimes;
 }
@@ -231,4 +295,188 @@ if ( function_exists( 'athena_sc_tinymce_init' ) ) {
 	add_filter( 'athena_sc_enable_tinymce_formatting', '__return_true' );
 }
 
-?>
+
+/**
+ * Allow special tags in post bodies that would get stripped otherwise for most users.
+ * Modifies $allowedposttags defined in wp-includes/kses.php
+ *
+ * http://wordpress.org/support/topic/div-ids-being-stripped-out
+ * http://wpquicktips.wordpress.com/2010/03/12/how-to-change-the-allowed-html-tags-for-wordpress/
+ **/
+$allowedposttags['input'] = array(
+	'type' => array(),
+	'value' => array(),
+	'id' => array(),
+	'name' => array(),
+	'class' => array()
+);
+$allowedposttags['select'] = array(
+	'id' => array(),
+	'name' => array()
+);
+$allowedposttags['option'] = array(
+	'id' => array(),
+	'name' => array(),
+	'value' => array()
+);
+$allowedposttags['iframe'] = array(
+	'type' => array(),
+	'value' => array(),
+	'id' => array(),
+	'name' => array(),
+	'class' => array(),
+	'src' => array(),
+	'height' => array(),
+	'width' => array(),
+	'allowfullscreen' => array(),
+	'frameborder' => array()
+);
+$allowedposttags['object'] = array(
+	'height' => array(),
+	'width' => array()
+);
+
+$allowedposttags['param'] = array(
+	'name' => array(),
+	'value' => array()
+);
+
+$allowedposttags['embed'] = array(
+	'src' => array(),
+	'type' => array(),
+	'allowfullscreen' => array(),
+	'allowscriptaccess' => array(),
+	'height' => array(),
+	'width' => array()
+);
+// Most of these attributes aren't actually valid for some of
+// the tags they're assigned to, but whatever:
+$allowedposttags['div'] =
+$allowedposttags['a'] =
+$allowedposttags['button'] = array(
+	'id' => array(),
+	'class' => array(),
+	'style' => array(),
+	'width' => array(),
+	'height' => array(),
+
+	'align' => array(),
+	'aria-hidden' => array(),
+	'aria-labelledby' => array(),
+	'autofocus' => array(),
+	'dir' => array(),
+	'disabled' => array(),
+	'form' => array(),
+	'formaction' => array(),
+	'formenctype' => array(),
+	'formmethod' => array(),
+	'formonvalidate' => array(),
+	'formtarget' => array(),
+	'hidden' => array(),
+	'href' => array(),
+	'name' => array(),
+	'rel' => array(),
+	'rev' => array(),
+	'role' => array(),
+	'target' => array(),
+	'type' => array(),
+	'title' => array(),
+	'value' => array(),
+
+	// Bootstrap JS stuff:
+	'data-dismiss' => array(),
+	'data-toggle' => array(),
+	'data-target' => array(),
+	'data-backdrop' => array(),
+	'data-spy' => array(),
+	'data-offset' => array(),
+	'data-animation' => array(),
+	'data-html' => array(),
+	'data-placement' => array(),
+	'data-selector' => array(),
+	'data-title' => array(),
+	'data-trigger' => array(),
+	'data-delay' => array(),
+	'data-content' => array(),
+	'data-offset' => array(),
+	'data-offset-top' => array(),
+	'data-loading-text' => array(),
+	'data-complete-text' => array(),
+	'autocomplete' => array(),
+	'data-parent' => array(),
+);
+
+
+/**
+ * Hide unused admin tools
+ **/
+function hide_admin_links() {
+	remove_menu_page( 'edit-comments.php' );
+}
+add_action( 'admin_menu', 'hide_admin_links' );
+
+
+/**
+ * Prevent Wordpress from trying to redirect to a "loose match" post when
+ * an invalid URL is requested.  WordPress will redirect to 404.php instead.
+ *
+ * See http://wordpress.stackexchange.com/questions/3326/301-redirect-instead-of-404-when-url-is-a-prefix-of-a-post-or-page-name
+ **/
+function no_redirect_on_404( $redirect_url ) {
+	if ( is_404() ) {
+		return false;
+	}
+	return $redirect_url;
+}
+add_filter( 'redirect_canonical', 'no_redirect_on_404' );
+
+
+/**
+ * Kill attachment, author, and daily archive pages.
+ *
+ * http://betterwp.net/wordpress-tips/disable-some-wordpress-pages/
+ **/
+function kill_unused_templates() {
+	global $wp_query, $post;
+
+	if ( is_author() || is_attachment() || is_day() || is_search() ) {
+		wp_redirect( home_url() );
+	}
+
+	if ( is_feed() ) {
+		$author     = get_query_var( 'author_name' );
+		$attachment = get_query_var( 'attachment' );
+		$attachment = ( empty( $attachment ) ) ? get_query_var( 'attachment_id' ) : $attachment;
+		$day        = get_query_var( 'day' );
+		$search     = get_query_var( 's' );
+
+		if ( !empty( $author ) || !empty( $attachment ) || !empty( $day ) || !empty( $search ) ) {
+			wp_redirect( home_url() );
+			$wp_query->is_feed = false;
+		}
+	}
+}
+add_action( 'template_redirect', 'kill_unused_templates' );
+
+
+/**
+ * Disable the Yoast SEO meta box on post types that we don't need it on
+ * (non-public-facing posts, i.e. Sections)
+ **/
+function remove_yoast_meta_boxes() {
+	$post_types = array(
+		'ucf_resource_link',
+		'ucf_section'
+	);
+	foreach ( $post_types as $post_type ) {
+		remove_meta_box( 'wpseo_meta', $post_type, 'normal' );
+	}
+}
+add_action( 'add_meta_boxes', 'remove_yoast_meta_boxes' );
+
+
+/**
+ * Remove paragraph tag from excerpts
+ **/
+remove_filter( 'the_excerpt', 'wpautop' );
+
