d957fd276096bda5e86433fbb52065736eeb86c5
Jo Dickson <jogreybill@gmail.com>
2017-06-22 14:08:47 -0500

    Split out meta-related functions into a separate file

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 include_once 'includes/utilities.php';
 include_once 'includes/config.php';
+include_once 'includes/meta.php';
 include_once 'includes/wp-bs-navwalker.php';
 include_once 'includes/header-functions.php';
 
