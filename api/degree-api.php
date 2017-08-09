e8a5497f5c683f9447421cfa546ea662b590639b
Jim Barnes <jimbarnesdeveloper@gmail.com>
2017-08-09 12:30:25 -0400

    Added logic for adding meta and taxonomy to the degree feed.

--- api/degree-api.php
+++ api/degree-api.php
@@ -1,36 +1,89 @@
 <?php
 /**
- * Adds an endpoint for querying degrees
- **/
-class Degree_API extends WP_REST_Controller {
-	public function register_routes() {
-		$version = 1;
-		$namespace = 'ucf/v' . $version;
-		$base = 'degrees';
-
-		register_rest_route( "$namespace/", $base, array(
-			array(
-				'methods'             => WP_REST_Server::READABLE,
-				'callback'            => array( 'Degree_API', 'get_degrees' ),
-				'permission_callback' => function() { return true; }
-			)
-		) );
-	}
-
-	public function get_degrees( $request ) {
-		$items = get_posts( array(
-			'post_type'      => 'degree',
-			'posts_per_page' => -1
-		) );
-
-		$retval = array();
-
-		foreach( $items as $item ) {
-			$retval[] = $item;
-		}
-
-		return new WP_REST_Response( $retval, 200 );
+ * Add REST API support to an already registered post type.
+ */
+function add_degree_endpoint() {
+	global $wp_post_types;
+	
+	//be sure to set this to the name of your post type!
+	$post_type_name = 'degree';
+	if( isset( $wp_post_types[ $post_type_name ] ) ) {
+		$wp_post_types[$post_type_name]->show_in_rest = true;
+		// Optionally customize the rest_base or controller class
+		$wp_post_types[$post_type_name]->rest_base = 'degrees';
+		$wp_post_types[$post_type_name]->rest_controller_class = 'WP_REST_Posts_Controller';
 	}
 }
 
-add_action( 'rest_api_init', array( 'Degree_API', 'register_routes' ) );
+add_action( 'init', 'add_degree_endpoint', 99 );
+
+function add_rest_fields() {
+	$taxonomies = get_object_taxonomies( 'degree' );
+
+	foreach( $taxonomies as $tax ) {
+		register_rest_field( 'degree',
+			$tax,
+			array(
+				'get_callback'    => 'degree_rest_get_taxonomies',
+				'update_callback' => null,
+				'schema'          => null
+			)
+		);
+	}
+
+	register_rest_field( 'degree',
+		'degree_meta',
+		array(
+			'get_callback'    => 'degree_rest_get_termmeta',
+			'update_callback' => null,
+			'schema'          => null
+		)
+	);
+}
+
+add_action( 'rest_api_init', 'add_rest_fields', 99 );
+
+/**
+ * Gets all terms set to the post for the provided taxonomy
+ * @author Jim Barnes
+ * @since 0.0.1
+ * @param $object array | The data object that will be returned to the Rest API
+ * @param $tax string | The field name, which is this case should be a taxonomy slug
+ * @param $request array | The request object, which includes get and post parameters
+ * @return mixed | In this case a WP_Term object.
+ **/
+function degree_rest_get_taxonomies( $object, $tax, $request ) {
+	$terms = wp_get_post_terms( $object['id'], $tax );
+	foreach( $terms as $term ) {
+		$meta = get_term_meta( $term->term_id );
+
+		foreach( $meta as $key=>$val ) {
+			if ( count( $val ) === 1 ) {
+				$meta[$key] = $val[0];
+			} 
+		}
+
+		$term->meta = $meta;
+	}
+	return $terms;
+}
+
+/**
+ * Adds the postmeta to the `meta` field.
+ * @author Jim Barnes
+ * @since 1.0.1
+ * @param $object array | The data object that will be returned to the Rest API
+ * @param $field_name string | The field name
+ * @param $request array | The request object, which includes get and post parameters
+ * @return mixed | In this case an array of postmeta.
+ **/
+function degree_rest_get_termmeta( $object, $field_name, $request ) {
+	$retval = array();
+	$postmeta = get_post_meta( $object['id'] );
+	foreach( $postmeta as $key => $val ) {
+		if ( substr( $key, 0, 6 ) === 'degree' ) {
+			$retval[$key] = $val[0];
+		}
+	}
+	return $retval;
+}
