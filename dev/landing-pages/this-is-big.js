1de8efcd3ab48924c0977aabf063e3ffa170ad6e
Keegan Berry <keeganberry@gmail.com>
2017-08-30 09:37:16 -0400

    finalize page layout  and design

--- dev/landing-pages/this-is-big.js
+++ dev/landing-pages/this-is-big.js
@@ -1,3 +1,4 @@
+/*
 function homePageMajorsList() {
   $('.top-majors-heading').on('click', function() {
     if ($(window).width() < 992) {
@@ -11,3 +12,41 @@ function init() {
 }
 
 $(init());
+*/
+
+var player, iframe;
+var $ = document.querySelector.bind(document);
+
+// init player
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '200',
+    width: '300',
+    videoId: 'gJ390e5sjHk',
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+// when ready, wait for clicks
+function onPlayerReady(event) {
+  var player = event.target;
+  iframe = $('#player');
+  setupListener();
+}
+
+function setupListener (){
+$('.play-video').addEventListener('click', playFullscreen);
+}
+
+function playFullscreen (){
+  player.playVideo();//won't work on mobile
+
+  var requestFullScreen = iframe.requestFullScreen || iframe.mozRequestFullScreen || iframe.webkitRequestFullScreen;
+  if (requestFullScreen) {
+    requestFullScreen.bind(iframe)();
+  }
+}
+
+$(setupListener());
