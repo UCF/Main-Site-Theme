28eeaf1671bb054e07c96923c85f41d29e9e4948
Jo Dickson <jogreybill@gmail.com>
2017-09-05 15:10:08 -0500

    Added meta field for custom header contents; added more header functions that return specific header parts; added logic to try to use a fallback -xs image in header media backgrounds if available

--- includes/header-functions.php
+++ includes/header-functions.php
@@ -8,21 +8,28 @@
  **/
 function get_header_images( $post ) {
 	$retval = array(
-		'header_image'    => get_field( 'page_header_image', $post->ID ),
-		'header_image_xs' => get_field( 'page_header_image_xs', $post->ID )
+		'header_image'    => '',
+		'header_image_xs' => ''
 	);
+
+	if ( $post->post_type === 'degree' ) {
+		$retval = degree_backup_headers( $post );
+	}
+
+	if ( $post_header_image = get_field( 'page_header_image', $post->ID ) ) {
+		$retval['header_image'] = $post_header_image;
+	}
+	if ( $post_header_image_xs = get_field( 'page_header_image_xs', $post->ID ) ) {
+		$retval['header_image_xs'] = $post_header_image_xs;
+	}
 
 	if ( $retval['header_image'] ) {
 		return $retval;
-	} else if( $post->post_type === 'degree' ) {
-		return degree_backup_headers( $post );
 	}
-
 	return false;
 }
 
 function degree_backup_headers( $post ) {
-
 	$college = wp_get_post_terms( $post->ID, 'colleges' );
 
 	if ( is_array( $college ) ) {
@@ -34,6 +41,7 @@ function degree_backup_headers( $post ) {
 		'header_image_xs' => get_field( 'page_header_image_xs', 'colleges_' . $college->term_id )
 	);
 }
+
 
 /**
  * Gets the header video sources for pages.
@@ -52,6 +60,36 @@ function get_header_videos( $post ) {
 	}
 
 	return false;
+}
+
+
+/**
+ * Returns title text for use in the page header.
+ **/
+ function get_header_title( $post ) {
+	$title = '';
+
+	if ( is_404() ) {
+		$title = '404 Not Found';
+	}
+	else {
+		$title = do_shortcode( get_field( 'page_header_title', $post->ID ) );
+	}
+
+	if ( !$title ) {
+		// Fall back to the post title
+		$title = $post->post_title;
+	}
+
+	return wptexturize( $title );
+}
+
+
+/**
+ * Returns subtitle text for use in the page header.
+ **/
+ function get_header_subtitle( $post ) {
+	return wptexturize( do_shortcode( get_field( 'page_header_subtitle', $post->ID ) ) );
 }
 
 
@@ -83,73 +121,150 @@ function get_nav_markup( $image=true ) {
 
 
 /**
- * Returns the markup for page headers.
+ * Returns markup for page header title + subtitles within headers that use a
+ * media background.
  **/
-function get_header_media_markup( $post ) {
-	$page_title = get_post_meta( $post->ID, 'page_header_title', true );
-	$title = ( ! empty( $page_title ) ) ? $page_title : $post->post_title;
-	$subtitle = get_post_meta( $post->ID, 'page_header_subtitle', true );
-	$videos = get_header_videos( $post );
-	$images = get_header_images( $post );
+function get_header_content_title_subtitle( $post ) {
+	$title = get_header_title( $post );
+	$subtitle = get_header_subtitle( $post );
+
+	ob_start();
+?>
+	<div class="header-content-inner d-flex h-75 align-items-center">
+		<div class="container">
+			<div class="d-inline-block bg-primary-t-1">
+				<h1 class="header-title"><?php echo $title; ?></h1>
+			</div>
+			<?php if ( $subtitle ) : ?>
+			<div class="clearfix"></div>
+			<div class="d-inline-block bg-inverse">
+				<div class="header-subtitle"><?php echo $subtitle; ?></div>
+			</div>
+			<?php endif; ?>
+		</div>
+	</div>
+<?php
+	return ob_get_clean();
+}
+
+
+/**
+ * Returns markup for page header custom content within headers that use a
+ * media background.
+ **/
+function get_header_content_custom( $post ) {
+	$content = get_field( 'page_header_content', $post->ID );
+
+	ob_start();
+
+	if ( $content ) {
+		echo $content;
+	}
+
+	return ob_get_clean();
+}
+
+
+/**
+ * Returns the markup for page headers with media backgrounds.
+ **/
+function get_header_media_markup( $post, $videos, $images ) {
+	$videos     = $videos ?: get_header_videos( $post );
+	$images     = $images ?: get_header_images( $post );
 	$video_loop = get_field( 'page_header_video_loop', $post->ID );
 
 	ob_start();
 
-	if ( $images || $videos ) :
-		$header_height = get_field( 'page_header_height', $post->ID );
+	$header_height = get_field( 'page_header_height', $post->ID );
 ?>
-		<div class="header-media <?php echo $header_height; ?> media-background-container mb-0 d-flex flex-column">
-			<?php
-			if ( $videos ) {
-				echo get_media_background_video( $videos, $video_loop );
+	<div class="header-media <?php echo $header_height; ?> media-background-container mb-0 d-flex flex-column">
+		<?php
+		// Display the media background (video + picture)
+
+		if ( $videos ) {
+			echo get_media_background_video( $videos, $video_loop );
+		}
+		if ( $images ) {
+			$bg_image_srcs = array();
+			switch ( $header_height ) {
+				case 'header-media-fullscreen':
+					$bg_image_srcs = get_media_background_picture_srcs( null, $images['header_image'], 'bg-img' );
+					$bg_image_src_xs = get_media_background_picture_srcs( $images['header_image_xs'], null, 'header-img' );
+
+					if ( isset( $bg_image_src_xs['xs'] ) ) {
+						$bg_image_srcs['xs'] = $bg_image_src_xs['xs'];
+					}
+
+					break;
+				default:
+					$bg_image_srcs = get_media_background_picture_srcs( $images['header_image_xs'], $images['header_image'], 'header-img' );
+					break;
 			}
-			if ( $images ) {
-				$bg_image_srcs = array();
-				switch ( $header_height ) {
-					case 'header-media-fullscreen':
-						$bg_image_src_xs = get_media_background_picture_srcs( $images['header_image_xs'], null, 'header-img' );
-						$bg_image_srcs_sm = get_media_background_picture_srcs( null, $images['header_image'], 'bg-img' );
-						$bg_image_srcs = array_merge( $bg_image_src_xs, $bg_image_srcs_sm );
-						break;
-					default:
-						$bg_image_srcs = get_media_background_picture_srcs( $images['header_image_xs'], $images['header_image'], 'header-img' );
-						break;
+			echo get_media_background_picture( $bg_image_srcs );
+		}
+		?>
+
+		<?php
+		// Display the site nav
+		echo get_nav_markup();
+		?>
+
+		<?php
+		// Display the inner header contents
+		?>
+		<div class="header-content">
+			<div class="header-content-flexfix">
+				<?php
+				if ( get_field( 'page_header_content_type', $post->ID ) === 'title_subtitle' ) {
+					echo get_header_content_title_subtitle( $post );
 				}
-				echo get_media_background_picture( $bg_image_srcs );
-			}
-			?>
-			<?php echo get_nav_markup(); ?>
-			<div class="header-content">
-				<div class="header-content-flexfix">
-					<div class="header-content-inner d-flex h-75 align-items-center">
-						<div class="container">
-							<div class="d-inline-block bg-primary-t-1">
-								<h1 class="header-title"><?php echo $title ?></h1>
-							</div>
-							<?php if ( $subtitle ) : ?>
-							<div class="clearfix"></div>
-							<div class="d-inline-block bg-inverse">
-								<div class="header-subtitle"><?php echo do_shortcode( $subtitle ); ?></div>
-							</div>
-							<?php endif; ?>
-						</div>
-					</div>
-				</div>
+				else {
+					echo get_header_content_custom( $post );
+				}
+				?>
 			</div>
-	<?php else : ?>
-		<?php echo get_nav_markup( false ); ?>
-		<div class="container">
-			<h1><?php the_title(); ?></h1>
 		</div>
+	</div>
 <?php
-	endif;
+	return ob_get_clean();
+}
+
+
+/**
+ * Returns the default markup for page headers without a media background.
+ **/
+ function get_header_default_markup( $post ) {
+	$title    = get_header_title( $post );
+	$subtitle = get_header_subtitle( $post );
+
+	ob_start();
+?>
+	<?php echo get_nav_markup(); ?>
+
+	<div class="container">
+		<h1 class="mt-3 mt-sm-4 mt-md-5 mb-3"><?php echo $title; ?></h1>
+
+		<?php if ( $subtitle ): ?>
+		<p class="lead mb-4 mb-md-5"><?php echo $subtitle; ?></p>
+		<?php endif; ?>
+	</div>
+<?php
 	return ob_get_clean();
 }
 
 
 function get_header_markup() {
 	global $post;
-	echo get_header_media_markup( $post );
+
+	$videos = get_header_videos( $post );
+	$images = get_header_images( $post );
+
+	if ( $videos || $images ) {
+		echo get_header_media_markup( $post, $videos, $images );
+	}
+	else {
+		echo get_header_default_markup( $post );
+	}
 }
 
 ?>
