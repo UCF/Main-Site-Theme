6fde60b9ef3a6611e0afdc698f77b802e101edd2
Jo Dickson <jogreybill@gmail.com>
2017-10-26 11:57:28 -0500

    Updated font and mobile styles; made issue thumbnails clickable

--- functions.php
+++ functions.php
@@ -301,6 +301,7 @@ function mainsite_events_display_classic( $content, $items, $args, $display_type
 
 add_filter( 'ucf_events_display_classic', 'mainsite_events_display_classic', 11, 4 );
 
+
 /**
  * Academic Calendar Main-Site Layout
  **/
@@ -400,6 +401,7 @@ function main_site_academic_calendar_add_layout( $layouts ) {
 
 add_filter( 'ucf_acad_cal_get_layouts', 'main_site_academic_calendar_add_layout', 10, 1 );
 
+
 /**
  * Main-Site Pegasus List Layout
  **/
@@ -440,26 +442,31 @@ function main_site_pegasus_list_content( $content, $items, $args ) {
 	ob_start();
 ?>
 	<!-- Featured Issue -->
-	<div class="row mb-4">
-		<div class="col-sm-3">
-			<img class="w-100" src="<?php echo $thumbnail_url; ?>">
-		</div>
-		<div class="col-sm-9">
+	<div class="row mb-4 mb-md-5">
+		<div class="col-sm-4">
 			<a class="h2 mb-2" href="<?php echo $issue_url; ?>" target="_blank">
+				<img class="w-100" src="<?php echo $thumbnail_url; ?>" alt="<?php echo $issue_title; ?>">
+			</a>
+		</div>
+		<div class="col-sm-8 mt-3 mt-sm-0">
+			<a class="h1 text-secondary" href="<?php echo $issue_url; ?>" target="_blank">
 				<?php echo $issue_title; ?>
 			</a>
-			<p class="text-muted font-condensed text-uppercase">Featured Story</p>
-			<a class="h4" href="<?php echo $cover_story_url; ?>" target="_blank">
+			<p class="mt-2 mt-md-4 mb-2 text-muted text-uppercase">Featured Story</p>
+			<a class="h3 font-slab-serif text-secondary" href="<?php echo $cover_story_url; ?>" target="_blank">
 				<?php echo $cover_story_title; ?>
 			</a>
-			<p class="font-weight-light font-italic mb-2"><?php echo $cover_story_blurb; ?></p>
+			<p class="mt-3 mb-4"><?php echo $cover_story_blurb; ?></p>
 			<a class="btn btn-primary" href="<?php echo $issue_url; ?>" target="_blank">
-				Read More
+				Read More<span class="sr-only"> from <?php echo $issue_title; ?></span>
 			</a>
 		</div>
 	</div>
+
+	<hr class="hidden-lg-up my-4 my-md-5">
+
 	<div class="row">
-	<?php foreach( $items as $item ) : 
+	<?php foreach( $items as $item ) :
 		$issue_url   = $item->link;
 		$issue_title = $item->title->rendered;
 		$cover_story = $item->_embedded->issue_cover_story[0];
@@ -471,7 +478,7 @@ function main_site_pegasus_list_content( $content, $items, $args ) {
 		$thumbnail_id = $item->featured_media;
 		$thumbnail = null;
 		$thumbnail_url = null;
-	
+
 		if ( $thumbnail_id !== 0 ) {
 			$thumbnail = $item->_embedded->{"wp:featuredmedia"}[0];
 			$thumbnail_url = $thumbnail->media_details->sizes->full->source_url;
@@ -480,21 +487,26 @@ function main_site_pegasus_list_content( $content, $items, $args ) {
 			$cover_story_blurb = $cover_story_description;
 		} else if ( $cover_story_subtitle ) {
 			$cover_story_blurb = $cover_story_subtitle;
-		}	
+		}
 	?>
-		<div class="col-sm-3">
-			<img class="w-100 mb-2" src="<?php echo $thumbnail_url; ?>">
-			<a class="h2" href="<?php echo $issue_url; ?>" target="_blank">
-				<?php echo $issue_title; ?>
-			</a>
-			<p class="mt-2 text-muted font-condensed text-uppercase">Featured Story</p>
-			<a class="h4" href="<?php echo $cover_story_url; ?>" target="_blank">
-				<?php echo $cover_story_title; ?>
-			</a>
-			<p class="font-weight-light font-italic mb-2"><?php echo $cover_story_blurb; ?></p>
-			<a class="btn btn-primary" href="<?php echo $issue_url; ?>" target="_blank">
-				Read More
-			</a>
+		<div class="col-lg-3 mb-4">
+			<div class="row">
+				<div class="col-3 col-lg-12 pr-0 pr-sm-3">
+					<a class="text-secondary" href="<?php echo $issue_url; ?>" target="_blank">
+						<img class="w-100 mb-3" src="<?php echo $thumbnail_url; ?>" alt="<?php echo $issue_title; ?>">
+					</a>
+				</div>
+				<div class="col-9 col-lg-12">
+					<a class="h3 text-secondary" href="<?php echo $issue_url; ?>" target="_blank">
+						<?php echo $issue_title; ?>
+					</a>
+					<p class="mt-2 mt-md-3 mb-1 small text-muted text-uppercase">Featured Story</p>
+					<a class="h5 font-slab-serif text-secondary mb-3" href="<?php echo $cover_story_url; ?>" target="_blank">
+						<?php echo $cover_story_title; ?>
+					</a>
+					<p class="my-2"><?php echo $cover_story_blurb; ?></p>
+				</div>
+			</div>
 		</div>
 	<?php endforeach; ?>
 
