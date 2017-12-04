9e96bddfce78518db4c1b15efe48ffdc25bc2933
Jo Dickson <jogreybill@gmail.com>
2017-12-04 14:53:51 -0600

    Moved chevron list styles to global typography.scss file; replaced some colleges-specific class styles with utility classes

--- taxonomy-colleges.php
+++ taxonomy-colleges.php
@@ -98,7 +98,7 @@ $colleges = get_terms( array( 'taxonomy' => 'colleges', 'hide_empty' => false ) 
 							Top College Degrees <span class="fa fa-chevron-circle-down" aria-hidden="true"></span>
 						</a>
 						<div id="top-degree-collapse" class="collapse d-lg-block">
-							<ul class="top-majors-list list-unstyled">
+							<ul class="top-majors-list list-unstyled ml-0 pl-4">
 								<?php echo display_top_degrees( $term ); // located in functions.php ?>
 							</ul>
 						</div>
@@ -114,19 +114,15 @@ $colleges = get_terms( array( 'taxonomy' => 'colleges', 'hide_empty' => false ) 
 	<?php endif; ?>
 	<section class="section-news">
 		<div class="container my-5">
-			<div class="row">
-				<div class="col-lg-8">
-					<h2><?php echo ( $news_title = get_field( 'colleges_alias', 'colleges_' . $term->term_id ) ) ? $news_title . " News" : $term->name . " News"; ?></h2>
+			<div class="row justify-content-between align-items-end">
+				<div class="col-auto">
+					<h2 class="mb-0"><?php echo ( $news_title = get_field( 'colleges_alias', 'colleges_' . $term->term_id ) ) ? $news_title . " News" : $term->name . " News"; ?></h2>
 				</div>
-				<div class="col-lg-4">
-					<a href="https://today.ucf.edu/topic/<?php echo $news_topic; ?>" class="more-stories float-lg-right text-uppercase text-default">Check out more stories <span class="fa fa-external-link text-primary" aria-hidden="true"></span></a>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-12">
-					<hr class="mt-0">
+				<div class="col-auto">
+					<a href="https://today.ucf.edu/topic/<?php echo $news_topic; ?>" class="h6 text-uppercase d-block mb-0 mt-2 text-default">Check out more stories <span class="fa fa-external-link text-primary" aria-hidden="true"></span></a>
 				</div>
 			</div>
+			<hr class="mt-2">
 			<div class="row">
 				<div class="col-md-8 col-sm-12">
 					<?php echo do_shortcode( '[ucf-news-feed title="" layout="modern" topics="' . $news_topic . '"]' ); ?>
