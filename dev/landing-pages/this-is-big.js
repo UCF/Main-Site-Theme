8dba4f5b8aa071faa613baffbf819465997c70c8
Jo Dickson <jogreybill@gmail.com>
2017-12-13 10:58:49 -0600

    Removed old v2 files

--- dev/landing-pages/this-is-big.js
+++
@@ -1,9 +0,0 @@
-function topMajorsToggle() {
-  $('.top-majors-heading').on('click', function() {
-    if ($(window).width() < 992) {
-      $('.top-majors-heading, .top-majors-content').toggleClass('expanded');
-    }
-  });
-}
-
-$(topMajorsToggle());
