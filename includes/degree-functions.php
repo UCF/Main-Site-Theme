626be5f0b96ce3db868724b7d20e57827cfba6db
RJ Bruneel <rj.bruneel@ucf.edu>
2020-04-23 14:02:21 -0400

    Code reivew updates

--- includes/degree-functions.php
+++ includes/degree-functions.php
@@ -525,27 +525,29 @@ function get_degree_quotes_modern_layout( $degree ) {
 	ob_start();
 
 	if ( have_rows( 'degree_quotes', $degree ) ) : ?>
-		<div class="jumbotron jumbotron-fluid bg-faded mb-0">
-			<div class="container">
-				<?php while ( have_rows( 'degree_quotes', $degree ) ) : the_row(); ?>
-					<div class="row">
-						<?php if( get_sub_field( 'degree_quote_image' ) ) : ?>
-							<div class="col-lg-3 text-center text-lg-right align-self-center">
-								<img src="<?php the_sub_field( 'degree_quote_image' ); ?>" class="img-fluid"
-									alt="<?php the_sub_field( 'degree_quote_image_alt' ); ?>">
+		<section>
+			<div class="jumbotron jumbotron-fluid bg-faded mb-0">
+				<div class="container">
+					<?php while ( have_rows( 'degree_quotes', $degree ) ) : the_row(); ?>
+						<div class="row">
+							<?php if( get_sub_field( 'degree_quote_image' ) ) : ?>
+								<div class="col-lg-3 text-center text-lg-right align-self-center">
+									<img src="<?php the_sub_field( 'degree_quote_image' ); ?>" class="img-fluid"
+										alt="<?php the_sub_field( 'degree_quote_image_alt' ); ?>">
+								</div>
+							<?php endif; ?>
+							<?php $quote_col_class = ( get_sub_field( 'degree_quote_image' ) ) ? "col-lg-9" : "col-12 col-xl-10 offset-xl-1"; ?>
+							<div class="<?php echo $quote_col_class; ?>">
+								<blockquote class="blockquote blockquote-quotation">
+									<p class="mb-0"><?php the_sub_field( 'degree_quote' ); ?></p>
+									<footer class="blockquote-footer"><?php the_sub_field( 'degree_quote_footer' ); ?></footer>
+								</blockquote>
 							</div>
-						<?php endif; ?>
-						<?php $quote_col_class = ( get_sub_field( 'degree_quote_image' ) ) ? "col-lg-9" : "col-12 col-xl-10 offset-xl-1"; ?>
-						<div class="<?php echo $quote_col_class; ?>">
-							<blockquote class="blockquote blockquote-quotation">
-								<p class="mb-0"><?php the_sub_field( 'degree_quote' ); ?></p>
-								<footer class="blockquote-footer"><?php the_sub_field( 'degree_quote_footer' ); ?></footer>
-							</blockquote>
 						</div>
-					</div>
-				<?php endwhile; ?>
+					<?php endwhile; ?>
+				</div>
 			</div>
-		</div>
+		</section>
 	<?php endif;
 
 	return ob_get_clean();
@@ -595,7 +597,7 @@ function get_degree_skills_career_modern_layout( $degree ) {
 					<?php if( $degree_careers_heading ) : ?>
 						<div class="col-lg-4 offset-lg-1 mt-4">
 							<?php if( $degree_careers_heading ) : ?>
-								<h3 class="font-condensed h5 text-uppercase mb-3"><?php echo $degree_careers_heading; ?></h2>
+								<h3 class="font-condensed h5 text-uppercase mb-3"><?php echo $degree_careers_heading; ?></h3>
 							<?php endif; ?>
 
 							<?php if( have_rows( 'degree_career_list', $degree ) ) : ?>
