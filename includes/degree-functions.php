6fb66686bfd7033e049b0f9ee6f56ae7bd0b57c5
RJ Bruneel <rj.bruneel@ucf.edu>
2020-04-23 16:03:40 -0400

    code review updates

--- includes/degree-functions.php
+++ includes/degree-functions.php
@@ -489,15 +489,18 @@ function get_degree_course_overview_modern_layout( $degree ) {
 						<div class="accordion" role="tablist" id="courses">
 							<?php while ( have_rows( 'course_overview', $degree ) ) : the_row(); ?>
 								<div class="accordion-courses mt-0 pt-0 pt-lg-3">
-									<h3 class="mb-0">
-										<a <?php if ( get_row_index() !== 1 ) echo 'class="collapsed"' ?> data-toggle="collapse" data-target="#course-<?php echo get_row_index(); ?>" aria-controls="course-<?php echo get_row_index(); ?>" role="button" tabindex="0" aria-expanded="true">
+									<a <?php if ( get_row_index() !== 1 ) echo 'class="collapsed"' ?>
+										data-toggle="collapse" data-target="#course-<?php echo get_row_index(); ?>"
+										aria-controls="course-<?php echo get_row_index(); ?>" role="button" tabindex="0" aria-expanded="true">
+										<h3 class="mb-0">
 											<span class="font-condensed h6 letter-spacing-2 mb-3 text-uppercase">
 												<?php the_sub_field( 'course_title' ); ?>
 											</span>
 											<span class="course-icon pull-right text-inverse-aw fa" aria-hidden="true"></span>
-										</a>
-									</h3>
-									<div class="collapse<?php if ( get_row_index() === 1 ) echo " show" ?>" id="course-<?php echo get_row_index(); ?>" data-parent="#courses" role="tabpanel" aria-expanded="true">
+										</h3>
+									</a>
+									<div class="collapse<?php if ( get_row_index() === 1 ) echo " show" ?>"
+										id="course-<?php echo get_row_index(); ?>" data-parent="#courses" role="tabpanel" aria-expanded="true">
 										<p class="mt-3 mb-0"><?php the_sub_field( 'course_description' ); ?></p>
 									</div>
 								</div>
@@ -505,19 +508,21 @@ function get_degree_course_overview_modern_layout( $degree ) {
 						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-12">
-						<a href="<?php echo $post_meta['degree_pdf']; ?>" rel="nofollow">
-						<?php
-						if( $course_catalog_link_text ) :
-							echo $course_catalog_link_text;
-						else :
-						?>
-							View all <?php echo get_header_title( $degree ); ?> Courses
-						<?php endif; ?>
-						</a>
+				<?php if( ! empty( $post_meta['degree_pdf'] ) ) : ?>
+					<div class="row">
+						<div class="col-12">
+							<a href="<?php echo $post_meta['degree_pdf']; ?>" rel="nofollow">
+							<?php
+							if( $course_catalog_link_text ) :
+								echo $course_catalog_link_text;
+							else :
+							?>
+								View all <?php echo get_header_title( $degree ); ?> Courses
+							<?php endif; ?>
+							</a>
+						</div>
 					</div>
-				</div>
+				<?php endif; ?>
 			</div>
 		</section>
 	<?php endif;
