e3054cb1cc9d49dbac417a4c680ebdb95945fdd8
Jo Dickson <jogreybill@gmail.com>
2020-08-28 11:16:02 -0400

    Added jump nav to phonebook results if more than one grouped section of results is returned

--- template-phonebook.php
+++ template-phonebook.php
@@ -11,6 +11,16 @@
 $query = isset( $_GET['query'] ) ? $_GET['query'] : '';
 $results = get_phonebook_results( $query );
 $phonebook_ctas = get_field( 'phonebook_ctas' );
+
+$groups_with_results = 0;
+if ( is_array( $results ) ) {
+	foreach ( $results as $key => $grouped_results ) {
+		if ( $key !== 'total_results' && count( $grouped_results ) > 0 ) {
+			$groups_with_results++;
+		}
+	}
+}
+
 ?>
 
 <div class="container mt-md-4 pb-4 pb-md-5">
@@ -37,49 +47,74 @@ $phonebook_ctas = get_field( 'phonebook_ctas' );
 				<p class="mb-0">No results were found.</p>
 			</div>
 			<?php else: ?>
-				<p class="lead mb-5"><?php echo $results['total_results']; ?> phonebook results found for &ldquo;<?php echo stripslashes( htmlentities( $query ) ); ?>&rdquo;</p>
+				<p class="lead"><?php echo $results['total_results']; ?> phonebook results found for &ldquo;<?php echo stripslashes( htmlentities( $query ) ); ?>&rdquo;</p>
 
-				<?php if ( count( $results['organizations'] ) ) : ?>
-				<h2 id="phonebook-organizations" class="h3 sticky-top pt-3 pb-4 bg-faded">
-					Organizations
-					<span class="small d-inline-block ml-1">(<?php echo count( $results['organizations'] ); ?> results)</span>
-				</h2>
-				<ul class="card list-unstyled mb-5">
-					<?php
-					foreach ( $results['organizations'] as $org ) {
-						echo format_phonebook_result( $org );
-					}
-					?>
-				</ul>
+				<?php if ( $groups_with_results > 1 ) : ?>
+				<nav class="d-sm-flex flex-sm-row align-items-sm-center" aria-label="Phonebook search result navigation">
+					<span class="d-inline-block mr-4 pr-2">Jump to results for:</span>
+					<ul class="nav px-2 px-sm-0">
+						<?php if ( count( $results['organizations'] ) ) : ?>
+						<li class="nav-item">
+							<a class="nav-link px-2" href="#phonebook-organizations">Organizations</a>
+						</li>
+						<?php endif; ?>
+						<?php if ( count( $results['departments'] ) ) : ?>
+						<li class="nav-item">
+							<a class="nav-link px-2" href="#phonebook-departments">Departments</a>
+						</li>
+						<?php endif; ?>
+						<?php if ( count( $results['staff'] ) ) : ?>
+						<li class="nav-item">
+							<a class="nav-link px-2" href="#phonebook-staff">Staff</a>
+						</li>
+						<?php endif; ?>
+					</ul>
+				</nav>
 				<?php endif; ?>
 
-				<?php if ( count( $results['departments'] ) ) : ?>
-				<h2 id="phonebook-departments" class="h3 sticky-top pt-3 pb-4 bg-faded">
-					Departments
-					<span class="small d-inline-block ml-1">(<?php echo count( $results['departments'] ); ?> results)</span>
-				</h2>
-				<ul class="card list-unstyled mb-5">
-					<?php
-					foreach ( $results['departments'] as $dept ) {
-						echo format_phonebook_result( $dept );
-					}
-					?>
-				</ul>
-				<?php endif; ?>
+				<div class="mt-4 mt-sm-5 pt-2 pt-sm-0">
+					<?php if ( count( $results['organizations'] ) ) : ?>
+					<h2 id="phonebook-organizations" class="h3 sticky-top pt-3 pb-4 bg-faded">
+						Organizations
+						<span class="small d-inline-block ml-1">(<?php echo count( $results['organizations'] ); ?> results)</span>
+					</h2>
+					<ul class="card list-unstyled mb-5">
+						<?php
+						foreach ( $results['organizations'] as $org ) {
+							echo format_phonebook_result( $org );
+						}
+						?>
+					</ul>
+					<?php endif; ?>
 
-				<?php if ( count( $results['staff'] ) ) : ?>
-				<h2 id="phonebook-staff" class="h3 sticky-top pt-3 pb-4 bg-faded">
-					Staff
-					<span class="small d-inline-block ml-1">(<?php echo count( $results['staff'] ); ?> results)</span>
-				</h2>
-				<ul class="card list-unstyled mb-5">
-					<?php
-					foreach ( $results['staff'] as $staff ) {
-						echo format_phonebook_result( $staff );
-					}
-					?>
-				</ul>
-				<?php endif; ?>
+					<?php if ( count( $results['departments'] ) ) : ?>
+					<h2 id="phonebook-departments" class="h3 sticky-top pt-3 pb-4 bg-faded">
+						Departments
+						<span class="small d-inline-block ml-1">(<?php echo count( $results['departments'] ); ?> results)</span>
+					</h2>
+					<ul class="card list-unstyled mb-5">
+						<?php
+						foreach ( $results['departments'] as $dept ) {
+							echo format_phonebook_result( $dept );
+						}
+						?>
+					</ul>
+					<?php endif; ?>
+
+					<?php if ( count( $results['staff'] ) ) : ?>
+					<h2 id="phonebook-staff" class="h3 sticky-top pt-3 pb-4 bg-faded">
+						Staff
+						<span class="small d-inline-block ml-1">(<?php echo count( $results['staff'] ); ?> results)</span>
+					</h2>
+					<ul class="card list-unstyled mb-5">
+						<?php
+						foreach ( $results['staff'] as $staff ) {
+							echo format_phonebook_result( $staff );
+						}
+						?>
+					</ul>
+					<?php endif; ?>
+				</div>
 			<?php endif; ?>
 
 		</div>
