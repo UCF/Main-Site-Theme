440b1222194b5cbdeda71122625dce647bbcdb9f
Jo Dickson <jogreybill@gmail.com>
2020-11-03 15:17:28 -0500

    Added field for setting highlight image alt text; re-grouped highlight fields; updated desc/highlights field descriptions

--- template-parts/degree/description/sidebar-curated.php
+++ template-parts/degree/description/sidebar-curated.php
@@ -17,16 +17,32 @@ if ( $post->post_type === 'degree' ) :
 
 		<?php if ( have_rows( 'highlights', $post ) ) : ?>
 		<h3 class="heading-underline mb-4 pb-2">Highlights</h3>
-			<?php while ( have_rows( 'highlights', $post ) ) : the_row(); ?>
-			<div class="row mb-4">
-				<div class="col-3 text-center">
-					<img src="<?php the_sub_field( 'highlight_image' ); ?>" class="img-fluid" alt="">
+			<?php
+			while ( have_rows( 'highlights', $post ) ) : the_row();
+				$highlight_image_data = get_sub_field( 'highlight' );
+				$highlight_image      = $highlight_image_data['image'] ?? '';
+				$highlight_image_alt  = $highlight_image_data['image_alt_text'] ?? '';
+				$highlight_copy       = get_sub_field( 'highlight_copy' );
+
+				if ( $highlight_copy ) :
+			?>
+				<div class="row mb-4">
+
+					<?php if ( $highlight_image ) : ?>
+					<div class="col-3 text-center">
+						<img src="<?php echo $highlight_image; ?>" class="img-fluid" alt="<?php echo $highlight_image_alt; ?>">
+					</div>
+					<?php endif; ?>
+
+					<div class="col-9 align-self-center">
+						<?php echo $highlight_copy; ?>
+					</div>
+
 				</div>
-				<div class="col-9 align-self-center">
-					<?php the_sub_field( 'highlight_copy' ); ?>
-				</div>
-			</div>
-			<?php endwhile; ?>
+			<?php
+				endif;
+			endwhile;
+			?>
 		<?php endif; ?>
 
 	</div>
