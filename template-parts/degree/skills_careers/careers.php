a7ddf6a1f61335b6696b9b999e73be2ce61bf5a9
Jo Dickson <jogreybill@gmail.com>
2020-11-09 14:42:01 -0500

    Added var for careers count

--- template-parts/degree/skills_careers/careers.php
+++ template-parts/degree/skills_careers/careers.php
@@ -2,10 +2,11 @@
 $post = isset( $post ) ? $post : get_queried_object();
 
 if ( $post->post_type === 'degree' ) :
-	$limit   = have_rows( 'degree_skills_list', $post ) ? 10 : 20;
-	$careers = main_site_get_degree_careers( $post, $limit );
+	$limit         = have_rows( 'degree_skills_list', $post ) ? 10 : 20;
+	$careers       = main_site_get_degree_careers( $post, $limit );
+	$career_expand = count( $careers ) > 10 ? true : false;
 ?>
-<div class="degree-careers <?php if ( count( $careers ) > 10 ) { ?>degree-careers-expandable<?php } ?>" id="career-opportunities-list">
+<div class="degree-careers <?php if ( $career_expand ) { ?>degree-careers-expandable<?php } ?>" id="career-opportunities-list">
 	<ul class="degree-career-list">
 		<?php foreach ( $careers as $career ) : ?>
 		<li class="degree-career-list-item">
@@ -14,7 +15,7 @@ if ( $post->post_type === 'degree' ) :
 		<?php endforeach; ?>
 	</ul>
 
-	<?php if ( count( $careers ) > 10 ) : ?>
+	<?php if ( $career_expand ) : ?>
 	<a class="degree-careers-expand btn btn-outline-inverse" href="#career-opportunities-list">
 		Show More
 		<span class="sr-only">Career Opportunities</span>
