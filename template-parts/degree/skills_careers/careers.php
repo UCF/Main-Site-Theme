adbbd25a52f500f5059351d1cc0a5c95ed765401
Jo Dickson <jogreybill@gmail.com>
2020-11-09 14:30:22 -0500

    Only show career expand btn when more than 10 careers are displayed

--- template-parts/degree/skills_careers/careers.php
+++ template-parts/degree/skills_careers/careers.php
@@ -5,7 +5,7 @@ if ( $post->post_type === 'degree' ) :
 	$limit   = have_rows( 'degree_skills_list', $post ) ? 10 : 20;
 	$careers = main_site_get_degree_careers( $post, $limit );
 ?>
-<div class="degree-careers" id="career-opportunities-list">
+<div class="degree-careers <?php if ( count( $careers ) > 10 ) { ?>degree-careers-expandable<?php } ?>" id="career-opportunities-list">
 	<ul class="degree-career-list">
 		<?php foreach ( $careers as $career ) : ?>
 		<li class="degree-career-list-item">
@@ -13,10 +13,13 @@ if ( $post->post_type === 'degree' ) :
 		</li>
 		<?php endforeach; ?>
 	</ul>
+
+	<?php if ( count( $careers ) > 10 ) : ?>
 	<a class="degree-careers-expand btn btn-outline-inverse" href="#career-opportunities-list">
 		Show More
 		<span class="sr-only">Career Opportunities</span>
 	</a>
+	<?php endif; ?>
 </div>
 <?php
 endif;
