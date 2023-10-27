b0329d5d44996d7d3cf68a2695ecfec4ade67265
Jim Barnes <jimbarnesdeveloper@gmail.com>
2023-10-27 09:55:40 -0400

    Updated some headings on the expert guide profile pages

--- template-parts/expert/marketing.php
+++ template-parts/expert/marketing.php
@@ -23,7 +23,7 @@ if ( $post->post_type === 'person' ) :
 	<section id="marketing" aria-labelledby="marketing-heading">
 		<div class="jumbotron jumbotron-fluid mb-0">
 			<div class="container">
-				<h2 class="font-condensed text-uppercase mb-4 mb-sm-5" id="marketing-heading">UCF Media</h2>
+				<h2 class="font-condensed text-uppercase mb-4 mb-sm-5" id="marketing-heading">UCF Media Resources</h2>
 				<div class="row">
 				<?php if ( $pegasus ) : ?>
 				<div class="col-md-6">
@@ -73,7 +73,7 @@ if ( $post->post_type === 'person' ) :
 		<?php if ( $videos ): ?>
 		<div class="jumbotron jumbotron-fluid bg-secondary mb-0">
 			<div class="container">
-				<h3 class="font-condensed text-uppercase mb-4 mb-sm-5">Videos</h3>
+				<h3 class="font-condensed text-uppercase mb-4 mb-sm-5">Related Videos</h3>
 				<div class="row">
 				<?php foreach( $videos as $video ) : ?>
 				<div class="col-md-6 col-lg-4">
