1ed079a509870f30dce08b6507d5b577ae6e09c0
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2025-04-29 14:27:13 -0400

    added the phonebook ctas.

--- template-phonebook.php
+++ template-phonebook.php
@@ -7,11 +7,12 @@
 
 <?php get_header(); the_post(); ?>
 
-	<div class="bg-faded py-4 py-sm5">
-		<div class="container d-flex">
-					<input class="form-control" type="text" id="gsc-input" placeholder="" />
-					<button class="btn btn-primary d-flex align-items-center" id="gsc-search-button"><span class="fa-sharp fa-solid fa-magnifying-glass mr-2"></span><span>Search</span></button>
-		</div>
+<?php $phonebook_ctas = get_field( 'phonebook_ctas' ); ?>
+
+
+	<div class="container d-flex">
+		<input class="form-control" type="text" id="gsc-input" placeholder="" />
+		<button class="btn btn-primary d-flex align-items-center" id="gsc-search-button"><span class="fa-sharp fa-solid fa-magnifying-glass mr-2"></span><span>Search</span></button>
 	</div>
 	<div class="container">
 		<div class="gcse-searchresults-only" data-gname="ucf-phonebook" data-queryParameterName="q"></div>
@@ -19,5 +20,10 @@
 
 	<script async src="https://cse.google.com/cse.js?cx=c41924672054e4f57&gname=ucf-phonebook"></script>
 
+	<?php if ( isset( $phonebook_ctas ) ) : ?>
+		<div class="container mb-4 mb-sm-5">
+			<?php echo $phonebook_ctas; ?>
+		</div>
+	<?php endif; ?>
 
 <?php get_footer(); ?>
