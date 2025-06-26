6ca7ffbdd1ab5b754cdf32bd507bd654fef493b2
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2025-06-26 13:49:36 -0400

    modified the markup to increase the readability.

--- template-parts/degree/quotes.php
+++ template-parts/degree/quotes.php
@@ -12,17 +12,13 @@ if ( $post->post_type === 'degree' ) :
 						<?php
 							$source = get_sub_field( 'degree_quote_image_source' );
 							$img_url = $source === 'upload' ? get_sub_field( 'degree_quote_image' ) : get_sub_field( 'degree_quote_image_url' );
+							$quote_col_class = ( $img_url ) ? "col-lg-9" : "col-12 col-xl-10 offset-xl-1";
 						?>
 						<?php if ( $img_url ) : ?>
 							<div class="col-lg-3 text-center text-lg-right align-self-center">
-								<img src="<?php echo $img_url ?>" class="img-fluid rounded-circle"
-									alt="<?php the_sub_field( 'degree_quote_image_alt' ); ?>">
+								<img src="<?php echo $img_url ?>" class="img-fluid rounded-circle" alt="<?php the_sub_field( 'degree_quote_image_alt' ); ?>">
 							</div>
 						<?php endif; ?>
-						<?php $quote_col_class = ( $img_url ) ?
-							"col-lg-9" :
-							"col-12 col-xl-10 offset-xl-1";
-						?>
 						<div class="<?php echo $quote_col_class; ?>">
 							<blockquote class="blockquote blockquote-quotation">
 								<p class="mb-0">
