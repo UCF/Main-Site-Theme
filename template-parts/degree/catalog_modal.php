f0074cee802bfe6ffe92ff0a84aa0d02c44f3d41
Ramin Farhadi <ra680091@MacBookPro.lan>
2025-08-18 13:07:45 -0400

    added the program title on top of the modal.

--- template-parts/degree/catalog_modal.php
+++ template-parts/degree/catalog_modal.php
@@ -3,14 +3,18 @@ $post = isset( $post ) ? $post : get_queried_object();
 
 if ( $post->post_type === 'degree' ) :
 	$catalog_desc_full = trim( get_field( 'degree_description_full', $post ) );
-
 	if ( $catalog_desc_full ) :
 ?>
 	<div class="modal fade" id="catalogModal" tabindex="-1" role="dialog" aria-label="Full Program Description" aria-hidden="true">
 		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
-				<div class="modal-header px-4 pt-4">
-					<button type="button" class="close ml-auto" data-dismiss="modal" aria-label="Close">
+				<div class="modal-header pl-4 pl-md-5 py-md-4">
+					<?php
+						if ( $post->post_title ) {
+							echo '<h2 class="h3 pt-4 font-condensed">' . esc_html( $post->post_title ) . '</h2>';
+						}
+					?>
+					<button type="button" class="close ml-auto pt-2 pr-3 pr-md-4" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
 				</div>
