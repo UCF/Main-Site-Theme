87cb54c690147d2ea6d8230a1485830415c8d241
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-08-26 08:56:27 -0400

    Added video template and header

---
+++ template-video.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Video Transcript
+ * Template Post Type: page
+ */
+?>
+
+<?php get_header(); the_post(); ?>
+
+<div class="video-template-content">
+	<?php echo the_content(); ?>
+</div>
+
+<?php get_footer(); ?>
