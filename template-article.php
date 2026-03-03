136cd67639734cd9debbd471bab759e2671f3f32
Jim Barnes <jimbarnesdeveloper@gmail.com>
2026-03-03 10:20:36 -0500

    Added related stories and personal responses

--- template-article.php
+++ template-article.php
@@ -6,7 +6,8 @@
 ?>
 
 <?php get_header(); the_post(); ?>
-<div class="container">
+<article>
+<div class="container mb-4">
 	<div class="row">
 	<?php
 	/**
@@ -36,12 +37,13 @@
 	 * This is the sidebar section
 	 */
 	$authors = get_field( 'author', $post->ID );
+	$coauthors = get_field( 'co-authors', $post->ID );
 	$references = get_field( 'references', $post->ID );
 
 	?>
 	<div class="col-md-4">
 	<?php if ( $authors ) : ?>
-	<aside class="card">
+	<aside class="card bg-faded mb-4">
 		<div class="card-block">
 			<h2 class="h3 heading-underline">Researchers in Focus</h2>
 			<?php if ( $authors ) : ?>
@@ -64,8 +66,95 @@
 		</div>
 	</aside>
 	<?php endif; ?>
+
+	<?php if ( $coauthors ) : ?>
+	<aside class="card bg-faded mb-4">
+		<div class="card-block">
+			<h2 class="h3 heading-underline">Co-Authors</h2>
+			<ul class="list-unstyled">
+			<?php foreach ( $coauthors as $coauthor ) : ?>
+				<li class="list-item mb-4"><?php echo $coauthor['co-author']; ?></li>
+			<?php endforeach; ?>
+			</ul>
+		</div>
+	</aside>
+	<?php endif; ?>
 	</div>
 	</div><!-- End .row -->
 </div>
+<?php
+/**
+ * Personal Response section
+ */
+$responses = get_field( 'personal_response', $post->ID );
+?>
+<aside>
+	<div class="container mb-4">
+		<div class="row">
+		<?php foreach ( $responses as $response ) : ?>
+		<div class="col-md-6">
+			<div class="card">
+				<div class="card-header bg-inverse">
+					<div class="row align-content-center">
+						<div class="col-1 col-sm-2 col-lg-1 align-content-center text-center"><i class="fa fa-question-circle fa-2x" aria-hidden="true"></i></div>
+						<div class="col"><p class="mb-0"> <?php echo $response['question']; ?></p></div>
+					</div>
+				</div>
+				<div class="card-block">
+					<p><?php echo $response['answer']; ?></p>
+				</div>
+			</div>
+		</div>
+		<?php endforeach; ?>
+		</div>
+	</div>
+</aside>
+</article>
+
+<?php
+/**
+ * Related stories
+ */
+
+$categories = get_the_category( $post->ID );
+
+$args = array(
+	'post_type'    => 'page',
+	'post__not_in' => array( $post->ID ),
+	'category_in'  => $categories
+);
+
+$related_stories = get_posts( $args );
+
+?>
+<aside class="jumbotron py-5 bg-faded">
+	<div class="container">
+		<div class="row">
+		<?php foreach ( $related_stories as $story ) :
+			$story_thumbnail = get_the_post_thumbnail( $story->ID, 'post-thumbnail', array(
+				'class' => 'card-img-top img-fluid'
+			) );
+			$story_title = get_field( 'title', $story->ID );
+			$story_subtitle = get_field( 'sub_title', $story->ID );
+			$excerpt = get_the_excerpt( $story );
+			$permalink = get_permalink( $story->ID );
+		?>
+			<div class="col-6 col-sm-4 col-md-3">
+				<div class="card mb-4">
+					<?php echo $story_thumbnail; ?>
+					<div class="card-block">
+						<h2 class="h5"><?php echo $story_title; ?></h2>
+						<p class="text-default small">
+							<a href="<?php echo $permalink; ?>" class="stretched-link">
+								<?php echo $story_subtitle; ?>
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		<?php endforeach; ?>
+		</div>
+	</div>
+</aside>
 
 <?php get_footer(); ?>
