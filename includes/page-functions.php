3eab7e5e2f3e9a5bbc84f637bb59e19713f98eef
Jim Barnes <jimbarnesdeveloper@gmail.com>
2026-03-18 17:48:38 -0400

    Updated logic to match degree template logic

--- includes/page-functions.php
+++ includes/page-functions.php
@@ -38,15 +38,10 @@ add_filter( 'manage_page_posts_columns', 'main_site_add_page_template_column', 1
  * @return string
  */
 function main_site_page_template_column_data( $column_name, $post_id ) {
-	if ( $column_name !== 'template_name' ) return;
-
-	$templates = wp_get_theme()->get_page_templates();
-
-	$value = get_page_template_slug( $post_id );
-
-	echo $value !== '' ?
-		$templates[$value] :
-		'Default';
+	$template_name_slug_map = wp_get_theme()->get_page_templates();
+	$template_slug = get_page_template_slug( $post_id );
+	$template_name = array_key_exists( $template_slug, $template_name_slug_map ) ? $template_name_slug_map[$template_slug] : 'Default';
+	echo $template_name;
 }
 
 add_action( 'manage_page_posts_custom_column', 'main_site_page_template_column_data', 10, 2 );
