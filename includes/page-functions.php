0a0e3c1e5c5eb5659effa0a83bd1095b453e0830
Jim Barnes <jimbarnesdeveloper@gmail.com>
2026-03-18 18:18:38 -0400

    Added logic to filter by page template

--- includes/page-functions.php
+++ includes/page-functions.php
@@ -38,6 +38,8 @@ add_filter( 'manage_page_posts_columns', 'main_site_add_page_template_column', 1
  * @return string
  */
 function main_site_page_template_column_data( $column_name, $post_id ) {
+	if ( $column_name !== 'template_name' ) return;
+
 	$template_name_slug_map = wp_get_theme()->get_page_templates();
 	$template_slug = get_page_template_slug( $post_id );
 	$template_name = array_key_exists( $template_slug, $template_name_slug_map ) ? $template_name_slug_map[$template_slug] : 'Default';
@@ -45,3 +47,86 @@ function main_site_page_template_column_data( $column_name, $post_id ) {
 }
 
 add_action( 'manage_page_posts_custom_column', 'main_site_page_template_column_data', 10, 2 );
+
+/**
+ * Adds the template_name column to the sortables
+ * @author Jim Barnes
+ * @since 3.34.2
+ * @param array $columns The array of sortable columns
+ *
+ * @return array
+ */
+function main_site_register_sortable_columns( $columns ) {
+    // 'column_slug' is the ID of your column; 'meta_key' is what we'll check in the query
+    $columns['template_name'] = 'template_name';
+    return $columns;
+}
+
+add_filter( 'manage_edit-page_sortable_columns', 'main_site_register_sortable_columns', 10, 1 );
+
+
+/**
+ * Creates a dropdown of template names
+ * and adds it to the page_template screen
+ * @author Jim Barnes
+ * @since 3.34.2
+ *
+ * @return void
+ */
+function main_site_add_template_filter() {
+	$screen = get_current_screen();
+
+    if ( $screen->post_type === 'page' ) {
+        $selected = $_GET['page_template'] ?? '';
+		$page_templates = wp_get_theme()->get_page_templates();
+
+		ob_start();
+?>
+	<select name="page_template">
+		<option value="">All Templates</option>
+		<?php foreach( $page_templates as $slug => $name ) : ?>
+		<option
+			value="<?php echo $slug; ?>"<?php echo $selected === $slug ? ' selected' : ''; ?>>
+				<?php echo $name; ?>
+		</option>
+		<?php endforeach; ?>
+	</select>
+<?php
+        echo ob_get_clean();
+    }
+}
+
+add_action( 'restrict_manage_posts', 'main_site_add_template_filter', 10, 0 );
+
+
+/**
+ * Sorts the results based on the _wp_page_template
+ * @author Jim Barnes
+ * @since 3.34.2
+ * @param WP_Query $query The incoming query object
+ *
+ * @return void
+ */
+function main_site_template_name_order_by( $query ) {
+    if ( ! is_admin() || ! $query->is_main_query() ) {
+        return;
+    }
+
+    if ( 'template_name' === $query->get( 'orderby' ) ) {
+        $query->set( 'meta_key', '_wp_page_template' );
+        $query->set( 'orderby', 'meta_value' ); // Use 'meta_value' for text
+    }
+
+	if ( ! empty( $_GET['page_template'] ) ) {
+
+		$query->set( 'meta_query', array(
+			array(
+				'key'     => '_wp_page_template',
+				'value'   => $_GET['page_template'],
+				'compare' => '='
+			)
+		) );
+	}
+}
+
+add_action( 'pre_get_posts', 'main_site_template_name_order_by', 10, 1 );
